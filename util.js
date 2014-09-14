6ab46ef3d6dce11291a42766f927145761907206
Access42 <aaltinier@access42.net>
2014-09-14 19:39:41 +0200

    Indicate license magnet to GPL3
    As recommanded by the Free Software Foundation in the LibreJS project: https://www.gnu.org/software/librejs/manual/html_node/Free-Licenses-Detection.html#Free-Licenses-Detection

--- util.js
+++ util.js
@@ -1,3 +1,5 @@
+// @license magnet:?xt=urn:btih:1f739d935676111cfff4b4693e3816e664797050&dn=gpl-3.0.txt GPL-v3-or-Later
+
 window.onload = function(){
 	TabList=document.querySelectorAll('A, BUTTON');
 	for (var i=0 ; i<TabList.length ; i++){
@@ -70,3 +72,4 @@ function getWindowHeight() {
     }
     return windowHeight;
 }
+// @license-end
